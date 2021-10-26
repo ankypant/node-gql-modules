b70591421c7bac64816addeca17eb449a63c7899
Ankit Pant <ankit.pant@klm.com>
2021-10-26 23:37:17 +0530

    added prettier, fixed package json, moved code to folders

---
+++ src/resolver/resolver.ts
@@ -0,0 +1,9 @@
+import { books } from '../data/books-data';
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+export const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
