36732d4f0b19eca51cf67d890d97df8413a8e83e
Ankit Pant <ankit.pant@klm.com>
2021-10-27 18:40:04 +0530

    added query and resolver for movies

--- src/query/query.ts
+++ src/query/query.ts
@@ -13,10 +13,16 @@ export const typeDefs = gql`
     author: String
   }
 
+  type Movies {
+    title: String
+    rating: String
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    movies: [Movies]
   }
 `;
--- src/resolver/resolver.ts
+++ src/resolver/resolver.ts
@@ -1,9 +1,10 @@
-import { books } from '../data/books-data';
+import { books, movieList } from '../data/mock.data';
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 export const resolvers = {
   Query: {
     books: () => books,
+    movies: () => movieList,
   },
 };
